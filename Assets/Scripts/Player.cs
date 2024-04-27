105f7c5a4024ffa25d6092d0ab8b36546fe6f1c5
Hamad Alaslani <Dasheightair@gmail.com>
2024-04-27 12:23:33 +0300

    Added gameplay functionality and some visuals
    created jump, double jump, slide, animations, background layers and parallax scrolling

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,53 +1,134 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
+    private Animator anim;
+
 
     [Header("Move info")]
-    public float moveSpeed;
-    public float jumpForce;
-    public bool runBegun;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float doubleJumpForce;
 
+    private bool canDoubleJump;
+    private bool PlayerUnlocked;
+
+    [Header("Slide info")]
+    [SerializeField] private float slideSpeed;
+    [SerializeField] private float slideTime;
+    [SerializeField] private float slideCooldown;
+    private float slideCooldownCounter;
+    private float slideTimeCounter;
+    private bool isSliding;
 
     [Header("Collision info")]
-    public float groundCheckDistance;
-    public LayerMask whatIsGround;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private Vector2 wallCheckSize;
     private bool isGrounded;
+    private bool wallDetected;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (runBegun)
-            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        slideTimeCounter -= Time.deltaTime;
+        slideCooldownCounter -= Time.deltaTime;
+
+        AnimatorControllers();
         CheckInput();
+        CheckForSlide();
         CheckCollision();
+
+        if (isGrounded)
+            canDoubleJump = true;
+
+        if (PlayerUnlocked && !wallDetected)
+            Movement();
+
+    }
+
+    private void CheckForSlide()
+    {
+        if (slideTimeCounter < 0)
+            isSliding = false;
+    }
+
+    private void Movement()
+    {
+        if (isSliding)
+            rb.velocity = new Vector2(slideSpeed, rb.velocity.y);
+
+        else 
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+    }
+
+    private void AnimatorControllers()
+    {
+        anim.SetFloat("xVelocity", rb.velocity.x);
+        anim.SetFloat("yVelocity", rb.velocity.y);
+
+        anim.SetBool("canDoubleJump", canDoubleJump);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isSliding", isSliding);
     }
 
     private void CheckCollision()
     {
         isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+        wallDetected = Physics2D.BoxCast(wallCheck.position, wallCheckSize, 0, Vector2.zero, 0, whatIsGround);
     }
 
     private void CheckInput()
     {
-        if (Input.GetButtonDown("Fire1") || Input.GetButtonDown("Fire2"))
-            runBegun = true;
+        if (Input.GetButtonDown("Fire2"))
+            PlayerUnlocked = true;
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        if (Input.GetButtonDown("Jump"))
+            JumpButton();
+        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.DownArrow))
+            SlideButton();
+
     }
 
+    private void SlideButton()
+    {
+        if(rb.velocity.x != 0 && slideCooldownCounter < 0){
+            isSliding = true;
+            slideTimeCounter = slideTime;
+            slideCooldownCounter = slideCooldown;
+        }
+    }
+
+    private void JumpButton()
+    {
+        if (isGrounded)
+        {
+            canDoubleJump = true;
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+        else if (canDoubleJump)
+        {
+            canDoubleJump = false;
+            rb.velocity = new Vector2(rb.velocity.x, doubleJumpForce);
+        }
+    }
     private void OnDrawGizmos()
     {
         Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - groundCheckDistance));
+        Gizmos.DrawWireCube(wallCheck.position, wallCheckSize);
     }
 }
